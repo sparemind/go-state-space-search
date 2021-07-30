97467fcd610c583fba270db0e8e916ae10a8fddb
Jake Chiang <jakewchiang@gmail.com>
2021-07-29 16:00:51 -1000

    Add path direction checks to test cases

--- astar_test.go
+++ astar_test.go
@@ -3,20 +3,32 @@ package astar_test
 import (
 	"fmt"
 	"github.com/sparemind/go-astar"
+	"strings"
 	"testing"
 )
 
-func assertSolutionCost(
+// assertSolution causes a test failure if the lowest cost solution for a
+// pathfinding scenario doesn't match the expected cost.
+//
+// This function can also accept optional arguments:
+// - A position, which will override any specified in the given world string.
+// - A string of transition directions representing the expected path, which will
+//   be checked against the found path.
+func assertSolution(
 	t *testing.T,
 	expectedCost float64,
 	worldString string,
-	args ...position,
+	args ...interface{},
 ) []astar.StateTransition {
 	world, start, goal := newWorld(worldString)
-	if len(args) > 0 {
-		start.x, start.y = args[0].x, args[0].y
-		if len(args) > 1 {
-			goal.x, goal.y = args[1].x, args[1].y
+
+	var expectedPathTransitions *string = nil
+	for _, arg := range args {
+		switch arg := arg.(type) {
+		case position:
+			goal.x, goal.y = arg.x, arg.y
+		case string:
+			expectedPathTransitions = &arg
 		}
 	}
 
@@ -28,21 +40,35 @@ func assertSolutionCost(
 		fmt.Println(world.SolutionString(solution))
 		t.Fatalf("expected solution with cost %f, found solution with cost %f", expectedCost, cost)
 	}
+	if pathTransitions := pathTransitions(solution); expectedPathTransitions != nil &&
+		pathTransitions != *expectedPathTransitions {
+		t.Fatalf("expected solution with directions %s, was %s",
+			*expectedPathTransitions,
+			pathTransitions)
+	}
 	return solution
 }
 
+func pathTransitions(path []astar.StateTransition) string {
+	builder := strings.Builder{}
+	for _, step := range path {
+		builder.WriteString(step.Transition.(string))
+	}
+	return builder.String()
+}
+
 func TestSearch_Search_NoSolution(t *testing.T) {
-	assertSolutionCost(t, -1, `
+	assertSolution(t, -1, `
 		..#..
 		@.#.*
 		..#..
 	`)
-	assertSolutionCost(t, -1, `
+	assertSolution(t, -1, `
 		...#
 		@.#*
 		...#
 	`)
-	assertSolutionCost(t, -1, `
+	assertSolution(t, -1, `
 		#...
 		@#.*
 		#...
@@ -50,39 +76,39 @@ func TestSearch_Search_NoSolution(t *testing.T) {
 }
 
 func TestSearch_EmptySolution(t *testing.T) {
-	path := assertSolutionCost(t, 0, `
+	path := assertSolution(t, 0, `
 		111
 		1@1
 		111
-	`, position{x: 1, y: 1}, position{x: 1, y: 1})
+	`, position{x: 1, y: 1})
 	if len(path) != 0 {
 		t.Fatalf("expected solution path to be empty, was %d states long", len(path))
 	}
 }
 
 func TestSearch_StraightPath(t *testing.T) {
-	assertSolutionCost(t, 6, `
+	assertSolution(t, 6, `
 		.@.....*.
-	`)
-	assertSolutionCost(t, 4, `
+	`, ">>>>>>")
+	assertSolution(t, 4, `
 		.....
 		@...*
 		.....
-	`)
-	assertSolutionCost(t, 4, `
+	`, ">>>>")
+	assertSolution(t, 4, `
 		.....
 		*...@
 		.....
-	`)
+	`, "<<<<")
 }
 
 func TestSearch_AvoidHighCost(t *testing.T) {
-	assertSolutionCost(t, 6, `
+	assertSolution(t, 6, `
 		.....
 		@.9.*
 		.....
 	`)
-	assertSolutionCost(t, 12, `
+	assertSolution(t, 12, `
 		@.594.*
 		..643..
 		..28...
@@ -91,25 +117,25 @@ func TestSearch_AvoidHighCost(t *testing.T) {
 }
 
 func TestSearch_TakeShortcut(t *testing.T) {
-	assertSolutionCost(t, 7, `
+	assertSolution(t, 7, `
 		@.4.*
 		..9..
 		.....
-	`)
-	assertSolutionCost(t, 10, `
+	`, ">>>>")
+	assertSolution(t, 10, `
 		@..23..*
 		...789..
 		...456..
 		........
-	`)
+	`, ">>>>>>>")
 }
 
 func TestSearch_LongPath(t *testing.T) {
-	assertSolutionCost(t, 18, `
+	assertSolution(t, 18, `
 		@..2..2....2...
 		.2.2..22.2...2.
-		...........2...
-		2.2.222222..222
+		.2.........2...
+		..2.222222..222
 		2.........2...*
-	`)
+	`, ">>VV>>>>>>>>V>V>>>")
 }
