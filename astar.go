88fde122267887cb2b6452110daba17c93f62a25
Jake Chiang <jakewchiang@gmail.com>
2021-07-30 20:47:45 -1000

    Improve documentation of Search and remove unused node field

--- astar.go
+++ astar.go
@@ -7,7 +7,11 @@ import (
 
 // Search returns the lowest cost path from a start state to a goal state
 // along with the path's total cost. It additionally returns a boolean flag
-// of whether a path could be found.
+// of whether a path could be found. The returned path consists of an ordered
+// slice of StateTransitions for all steps in the solution path, except for
+// the goal, starting from the start state. Each step consists of the state,
+// the transition to the next state in the path, and the cost of making the
+// transition.
 func Search(start State, goal State) ([]StateTransition, float64, bool) {
 	nodes := make(map[State]*node)
 	openSet := make(PriorityQueue, 0)
@@ -40,7 +44,6 @@ func Search(start State, goal State) ([]StateTransition, float64, bool) {
 				nextNode.estimatedPathCost = costFromStart + next.State.EstimateCost(&goal)
 				nextNode.costFromStart = costFromStart
 				nextNode.parent = current
-				nextNode.transition = next.Transition
 				if exists {
 					heap.Fix(&openSet, nextNode.index)
 				} else {
@@ -53,7 +56,7 @@ func Search(start State, goal State) ([]StateTransition, float64, bool) {
 }
 
 func reconstructPath(goal *node) []StateTransition {
-	// Find all states in the solution path
+	// Find all nodes in the solution path
 	goalToStartPath := make([]*node, 0)
 	current := goal
 	for current != nil {
@@ -61,6 +64,11 @@ func reconstructPath(goal *node) []StateTransition {
 		current = current.parent
 	}
 
+	// Reconstruct the path from start to goal. Since NextStates()
+	// should be a cheap operation we run it here to get the
+	// transition and cost of each step (as opposed to saving them
+	// in the main Search() as part of the nodes), which reduces
+	// the memory usage of nodes.
 	path := make([]StateTransition, 0, len(goalToStartPath))
 	for i := len(goalToStartPath) - 1; i > 0; i-- {
 		current := goalToStartPath[i]
